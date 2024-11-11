d46ca3d9dccda221f85b3a50a9596aca37afd5de
Andrey Zashchepin <tishina-az@mail.ru>
2024-11-11 14:20:46 +0500

    Добавление методов в класс Category

--- src/category.py
+++ src/category.py
@@ -1,14 +1,35 @@
+from itertools import product
+
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
     products: list
 
     categories_count = 0
-    products_count = 0
+    product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.categories_count += 1
-        Category.products_count += len(products)
+        Category.product_count += len(products)
+
+    @property
+    def products(self):
+        products_str = ""
+        for prod in self.__products:
+            products_str += f"{prod.name}, {prod.price} руб. Остаток: {prod.quantity} шт.\n"
+        return products_str
+
+    @property
+    def add_product(self):
+        return self.__products
+
+    @add_product.setter
+    def add_product(self, new_product: Product):
+        self.__products.append(new_product)
+        Category.product_count += 1
